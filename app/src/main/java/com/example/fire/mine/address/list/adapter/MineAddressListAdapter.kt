f1bfa53ad421f388189f32daaae6b932064d2e3b
wz2793312458 <wz2793312458@163.com>
2018-10-25 10:00:17 +0800

    代码提交

--- app/src/main/java/com/example/fire/mine/address/list/adapter/MineAddressListAdapter.kt
+++ app/src/main/java/com/example/fire/mine/address/list/adapter/MineAddressListAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.example.fire.R
 import com.example.fire.mine.address.entity.MineAddressData
 import kotlinx.android.synthetic.main.item_address.view.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 /**
  * @author by 王小智
@@ -26,6 +27,9 @@ class MineAddressListAdapter : RecyclerView.Adapter<MineAddressListAdapter.MineA
     override fun onBindViewHolder(holder: MineAddressListViewHolder, p1: Int) {
         val data = list[p1]
         holder.itemView.tvName.text = data.address
+        holder.itemView.tvDelete.onClick {
+            listener!!.delete(p1)
+        }
     }
 
 
@@ -44,4 +48,15 @@ class MineAddressListAdapter : RecyclerView.Adapter<MineAddressListAdapter.MineA
     }
 
     inner class MineAddressListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private var listener: onKotlinItemClickListener? = null
+
+    fun setOnItemClickListener(listener: onKotlinItemClickListener) {
+        this.listener = listener
+    }
+
+    interface onKotlinItemClickListener {
+
+        fun delete(position: Int)
+    }
 }
