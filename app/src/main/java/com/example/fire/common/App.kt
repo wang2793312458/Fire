34e0a32280063fef870cb4333048827b2c86461b
wz2793312458 <wz2793312458@163.com>
2018-11-11 15:11:35 +0800

    新增application

---
+++ app/src/main/java/com/example/fire/common/App.kt
@@ -0,0 +1,15 @@
+package com.example.fire.common
+
+import android.app.Application
+import kotlin.properties.Delegates
+
+class App : Application() {
+    companion object {
+        var instance: App by Delegates.notNull()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
