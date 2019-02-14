4dc911e17e949154a08b21830daaa163ca9dd76c
Android <mr_rough@163.com>
2019-02-14 16:40:06 +0800

    修改项目名称，在loginData里添加原接口返回层级

--- app/src/main/java/com/example/fire/login/entity/LoginData.kt
+++ app/src/main/java/com/example/fire/login/entity/LoginData.kt
@@ -7,4 +7,32 @@ data class LoginData(
         val userName: String,
         val userId: Int,
         val token: String
-)
+){
+    /**
+     *
+     {
+    "msg":"",
+    "res":{
+    "code":40000,
+    "data":{
+    "uerName":"15098725394",
+    "RyToken":"G0TKQlOADit6M24WCeWw==",
+    "trendsUserId":"545641217563676672",
+    "token":"0f1f0d979d60692d95b5b2272731a50b",
+    "hxPwd":null,
+    "hander":"eyJ0eXBlAFXbaNts",
+    "userId":49,
+    "userName":"用户_150****5394",
+    "backgroundImage":"",
+    "RyUserId":"user49",
+    "reminderTime":10,
+    "headPic":"",
+    "hxUser":null
+    },
+    "msg":""
+    },
+    "state":1
+    }
+
+     */
+}
